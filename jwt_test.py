065947f68df475b1c2335b3eed6a343e43cef848
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-04-03 08:50:26 +0100

    lvl2 update

--- jwt_test.py
+++ jwt_test.py
@@ -6,13 +6,20 @@ SECRET = "mybankai_secret_key_32bytes_long"
 ALGORITHM = "HS256"
 
 
+payload = {
+        "user_id" : 1,
+        "name" : "daramz"
+    }
+token = jwt.encode(payload,  SECRET, algorithm=ALGORITHM)
+
+token2 = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
 
 
 @app.get("/get-token")
 def get_token():
-    payload = {
-        "user_id" : 1,
-        "name" : "daramz"
-    }
-    token = jwt.encode(payload,  SECRET, algorithm=ALGORITHM)
     return {"token": token}
+
+@app.get("/get_token2")
+def get_token2():
+    
+    return {"Decoded": token2}
