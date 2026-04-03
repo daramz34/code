cfacb8956b006bccc669b35b80b995a6bcb45805
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-04-03 09:35:38 +0100

    lvl 3

--- jwt_test.py
+++ jwt_test.py
@@ -1,25 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt
 
 app = FastAPI()
 SECRET = "mybankai_secret_key_32bytes_long"
 ALGORITHM = "HS256"
 
+securiy = HTTPBearer()
 
-payload = {
-        "user_id" : 1,
-        "name" : "daramz"
-    }
-token = jwt.encode(payload,  SECRET, algorithm=ALGORITHM)
 
-token2 = jwt.decode(token, SECRET, algorithms=[ALGORITHM])
+def check_token(credentials: HTTPAuthorizationCredentials = Depends(securiy)):
+    try:
+        payload = jwt.decode(credentials.credentials, SECRET, algorithms=ALGORITHM)
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
 
 
 @app.get("/get-token")
 def get_token():
+    payload = {
+        "user_id" : 1,
+        "name" : "daramz"
+    }
+    token = jwt.encode(payload,  SECRET, algorithm=ALGORITHM)
     return {"token": token}
 
-@app.get("/get_token2")
-def get_token2():
+@app.get("/secret")
+def secret_route(current_user: dict = Depends(check_token)):
     
-    return {"Decoded": token2}
+    return {"Message": "You are in",
+            "user": current_user}
