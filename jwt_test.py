6dc2d53f2d3556ab6829786c82d69ca19755a495
Darasimi Rotimi <darasimirotimi@gmail.com>
2026-04-05 04:31:01 +0100

    lvl4

--- jwt_test.py
+++ jwt_test.py
@@ -1,36 +1,56 @@
 from fastapi import FastAPI, Depends, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from pydantic import BaseModel
 import jwt
 
 app = FastAPI()
-SECRET = "mybankai_secret_key_32bytes_long"
+
+
+SECRET = "my_bankai_secret_key_32bytes_long"
 ALGORITHM = "HS256"
 
-securiy = HTTPBearer()
+security = HTTPBearer()
+users = {
+    "daramz": "password1222",
+    "john": "john2929"
+}
 
 
-def check_token(credentials: HTTPAuthorizationCredentials = Depends(securiy)):
+
+class LoginData(BaseModel):
+    username: str
+    password: str
+
+
+
+
+def check_token(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
-        payload = jwt.decode(credentials.credentials, SECRET, algorithms=ALGORITHM)
+        payload = jwt.decode(credentials.credentials, SECRET, algorithms=[ALGORITHM])
         return payload
     except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=401, detail="Token expired")
+        raise HTTPException(status_code=401, detail="Token Expired")
     except jwt.InvalidTokenError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
 
 
 
-@app.get("/get-token")
-def get_token():
-    payload = {
-        "user_id" : 1,
-        "name" : "daramz"
-    }
-    token = jwt.encode(payload,  SECRET, algorithm=ALGORITHM)
-    return {"token": token}
+@app.post("/")
+def login(data: LoginData):
+    if data.username not in users:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    
+    if users[data.username] != data.password:
+        raise HTTPException(status_code=401, detail="Invalid username or password")
+    
+    token = jwt.encode({"username": data.username}, SECRET, algorithm=ALGORITHM)
+    return {"access_token": token, 
+            "token_type": "bearer"}
 
 @app.get("/secret")
 def secret_route(current_user: dict = Depends(check_token)):
-    
-    return {"Message": "You are in",
-            "user": current_user}
+    return {
+        "message": "You are in",
+        "user": current_user["username"]
+    }
